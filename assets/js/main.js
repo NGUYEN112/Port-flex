269247ff60a30bfa9cf1112d69254ebdd9929051
NguyenDGWA <nguyen-pnh@dgw.asia>
2024-03-13 14:57:31 +0700

    update animation for home & about us page

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,4 +10,35 @@ window.onload = function() {
         $(".container__inner").addClass("limitSide")
         $(".closeSideBar").addClass("show")
     })
+
+
+    window.addEventListener('hashchange', function() {
+        checkHash() 
+    });
+    
+    function checkHash() {
+        switch (window.location.hash) {
+            case "#about-us":
+                $(".content").addClass("hiddenContent")
+                setTimeout(() => {
+                    this.document.querySelector(".aboutContent").classList.remove("hiddenContent")
+                    this.document.querySelector(".aboutContent").classList.add("activeAni")
+                },750);
+                break;
+            default: 
+                $(".content").addClass("hiddenContent")
+                setTimeout(() => {
+                    this.document.querySelector(".homeContent").classList.remove("hiddenContent")
+                    this.document.querySelector(".homeContent").classList.add("activeAni")
+                },750);
+                break;
+        }
+    }
+
+    document.querySelector(".container__inner").classList.add("opening")
+    setTimeout(() => {
+        checkHash()
+    },500);
+    clearTimeout()
 };
+
